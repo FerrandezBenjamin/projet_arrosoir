1d005a4e67d53f150d9bb1e97ef98d83fc3d867f
FerrandezBenjamin <58741067+FerrandezBenjamin@users.noreply.github.com>
2020-05-15 14:04:35 +0200

    update

--- device/arro.js
+++ device/arro.js
@@ -1,38 +1,63 @@
 var temperature = 30; // La valeur de la température ambiante
 var donneesCapt = 31; // La valeur renvoyé du capteur d'humidité
 var meteo = false; // False = SOLEIL, true = PLUIE
-var caArrose = true; // Variable pour renvoyer quand ça arrose
+var caArrose = true; // false = PAS arrose, true = arrose
+const mqtt = require("mqtt");
+const client = mqtt.connect("mqtt://broker.hivemq.com");
+
+client.on("connect", () => {
+  client.subscribe("campusnum/jardin/weather");
+  client.subscribe("campusnum/jardin/hum");
+});
+
+client.on("message", function (topic, msg) {
+  switch (topic) {
+    case "campusnum/jardin/weather":
+      if (msg != "rain") {
+        meteo = false;
+      } else meteo = true;
+      jardinClean(meteo);
+      break;
+    case "campusnum/jardin/hum":
+      donneesCapt = msg;
+      jardinClean(meteo);
+      break;
+    default:
+      break;
+  }
+});
 
 jardinClean(meteo);
 
 // ---- function principal arrosage du jardin ----//
 
 function jardinClean(meteo) {
-  if (meteo == true) {
-    // Si il pleut, pas besoin d'arroser.
+  if (meteo) {
+    // Si il pleut, pas besoin d'arroser
     console.log("Il pleut, pas besoin d'arroser");
-  } else if (meteo == false) {
+    client.publish("campusnum/jardin/arro", 0);
+  } else if (!meteo) {
     // Si il pleut pas, prend en compte les données du capteur, puis arrose en conséquence
     if (donneesCapt >= 30) {
       arrosage(meteo);
       console.log(caArrose);
     }
   } else {
-    jardinClean(meteo); // Redémarre en boucle la fonction principal;
-    console.log(caArrose);
+    setInterval(() => {
+      jardinClean();
+    }, 10000); // Redémarre en boucle la fonction principal;
   }
 }
 
 // ---- function arrosage du jardin ----//
 function arrosage(meteo) {
-  if (meteo == true) {
+  if (meteo) {
     // Arrête la function si il se met à pleuvoir
-    console.log("STOP IL PLEUT");
-  } else if (meteo == false) {
+    console.log("STOP IL CE MET A PLEUVOIR");
+    client.publish("campusnum/jardin/arro", 0);
+  } else if (!meteo) {
     // Il pleut pas, ça arrose
     console.log("Doucement, ça arrose");
-    caArrose = false; // Permet de renvoyer la valeur pour savoir quand ça arrose ou non
+    client.publish("campusnum/jardin/arro", 1);
   }
 }
-
-function prevision(chezpo) {}
